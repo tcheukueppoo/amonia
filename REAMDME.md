6e341225859e245c3c8c6c61326d1e4dfe3df236
tcheukueppo <tcheukueppo@gmail.com>
2021-12-20 16:06:55 +0100

    improved dwm's autostart script

--- REAMDME.md
+++ REAMDME.md
@@ -1,1 +1,29 @@
-# Kueppo's dotfiles
+# AMONIA
+
+Amonia an sh toxic script which aims to setup a developement
+environment for most of the programming langauges and technologies,
+install and configure minimal tools, and applications.
+
+**Supported operating systems**
+OpenBSD, NetBSD, FreeBSD, ArchLinux(+ it derivatives),
+Debian(+ it derivatives), NixOS, and Alpine.
+
+## Command list mininal applications
+
+git, fzy, tmux, dict, scrot, ffmpeg,
+fswebcam, paranoid, pandoc, cups,
+gnuplot, vis, vim, nvim, kak, sfm, 
+
+
+## List of pragramming languages
+
+c, c++, sh/bash, perl, golang, raku,
+javascript, typescript, lua and python.
+
+## Gui applications
+
+dwm, dwl, dmenu, wayst, st, sent, slock, sad, 
+conky, telegram desktop, surf, firefox, flameshot,
+vimb, luakit, krita, gimp, and 
+
+## 
--- dots/dwm/autostart.sh
+++ dots/dwm/autostart.sh
@@ -1,39 +1,47 @@
 #!/usr/bin/env sh
 
-# setting monitor(s), there's is a condition because they may
-# be one monitor.
-outputs="$(xrandr --query | fgrep ' connected ' | \
-			sed -n 's/\([-[:alnum:]]\+\).*/\1/p')"
+# set background image
+backimg() {
+	test -x "$HOME/.fehbg" && . "$HOME/.fehbg"
+}
 
-# don't set anything if there's only one monitor
-if test $(echo "$outputs" | wc -l) != 1; then
-	first=true
+# starting conky
+conk() {
+	conkf="$HOME/.config/conky"
+	if [ -d "$conkf" ]; then
+		for i in "$conkf"; do
+			test -f "$i" && conky -c "$i"
+		done
+	fi
+}
+
+# setting monitor(s)
+output() {
+	outputs="$(xrandr --query | fgrep ' connected ' | \
+		sed -nE 's/([-[:alnum:]]+).*/\1/p')"
+	nout=$(echo "$outputs" | wc -l)
+	
+	# only change gamma if there's only one monitor
+	if [ $nout -eq 1 ]; then
+		xrandr --output "$outputs" --brightness 0.6
+		return
+	fi
+	cont=true
 	echo "$outputs" | \
 	while read out; do
-		if test $first = false; then
-			xrandr --output $out --auto --rotate \
-				normal --right-of $prev
+		if [ $cont = false ]; then
+			xrandr --output $out --auto --rotate normal \
+				--right-of $prev
 		else
 			xrandr --output $out --auto --rotate normal
-			first=false
+			cont=false
 		fi
 		prev="$out"
 	done
-else
-	xrandr --output $outputs --brightness 0.8
-fi
+	# set brightness
+	# command -v xbacklight && xbacklight -set 40
+}
 
-# set brightness
-# command -v xbacklight && xbacklight -set 40
-
-# set background image
-command -v feh && test -f "$HOME"/.background \
-&& feh --bg-fill "$HOME"/.background 
-
-# starting conky
-conkf="$HOME/.config/conky"
-if test -d $conkf; then
-	for i in $conkf; do
-		test -f "$i" && conky -c "$i"
-	done
-fi
+output &
+conk &
+backimg &
