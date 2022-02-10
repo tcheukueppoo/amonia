fc720a3aab472be70854d318f924d948460d2621
tcheukueppo <tcheukueppo@gmail.com>
2022-02-10 18:51:06 +0100

    xinitrc, vimrc, tmux, bashrc dwm's autostart script

--- dots/dwm/autostart.sh
+++ dots/dwm/autostart.sh
@@ -1,48 +1,34 @@
 #!/bin/sh
-#
-# See License file for copyright and license details.
 
-func_set_wallpaper ()
+# set_wallpaper
+# -------------
+set_wallpaper ()
 {
 	test -x "$HOME/.fehbg" && . "$HOME/.fehbg"
 }
+set_wallpaper &
 
-func_set_outputs ()
+# set_outputs
+# -----------
+# set outputs x1, x2, ...., xn where xn is right of xn-1
+set_outputs ()
 {
 	display=
 	pdisplay=
 	outputs=$( xrandr --listmonitors | tail -n +2 | awk -F[' *'] '{print $4}' | tr '\n' ':' )
 
-	test -z "$outputs" && return
+	test -n "$outputs" || return
 	pdisplay="${outputs%%:*}"
 	xrandr --output "$pdisplay" --auto --rotate normal
-	xrandr --output "$pdisplay" --brightness 0.85
+	xrandr --output "$pdisplay" --brightness 0.80
 	outputs="${outputs#*:}"
 	if test -n "$outputs"; then
 		IFS=$':'
 		for display in $outputs; do
 			xrandr --output "$display" --auto --rotate normal --right-of "$pdisplay"
-			xrandr --output "$display" --brightness 0.85
+			xrandr --output "$display" --brightness 0.80
 			pdisplay="$display"
 		done
 	fi
 }
-
-func_run_conky ()
-{
-	conk_theme=
-	conkfigs="$HOME/.config/conky"
-
-	for conk_theme in "$conkfigs"; do
-		test -f "$conk_theme" && conky -c "$conk_theme"
-	done
-}
-
-# The order is significant and the success of one function
-# may depend its previous one, so No Need to put them in
-# backgroud else it may have some annoying visual effects.
-{
-	func_set_outputs
-	func_set_wallpaper
-	func_run_conky
-}
+set_outputs &
